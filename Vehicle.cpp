d88c95f8d7a3c8e9dc86a49a374438546a936ad6
linadiaz <linisdr1@gmail.com>
2021-11-28 22:04:03 -0500

    created menu
    Added menu options to get information from the user

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,7 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "Vehicle.h"
 
-int main()
-{
+using namespace std;
+
+class car : public Vehicle{
+	
+
+};
+
+
+main(){
+
+	car c; // se instancia la clse vehicle
+	system("cls");
+	cout << "Vehicle Program" <<endl;
+	cout<<endl;
+	cout <<" Vehicle"<<endl;
+	string manu; // variable para pedir manufactura
+	int ano;  // variable para el aÒo de manufact
+
+	cout <<" Enter the manufacturer "<<endl;
 	
 	
+	cin >> manu;
+
+	cout << " Enter de year build "<<endl;
+	
+	cin >>ano;
+
+	// cargamos datos con setter
+	c.setVehicle(ano,manu);
+	// display information de vehicle
+	c.displayInfo();
+
+	system("pause");
+	return 0;
 }
