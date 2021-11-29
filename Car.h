472a11b41225da27f2afb92fa7c0aff2a75a790c
linadiaz <linisdr1@gmail.com>
2021-11-28 22:01:33 -0500

    Changed structure
    Added constructor, set, get

--- Car.h
+++ Car.h
@@ -1,6 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+
 class car {
     public: 
-int numberdoors;
+
+		int door;
+
+	public:
+		car(); // constgructor vacio
+		// seter asignamos valores
+		void setCar(int);
+		// geter retorna valores
+		int getDoor();
+		
+		void displayInfo();
+
+
+};
+Vehicle::Vehicle(){ // empty constructor
+}
+// seter asignamos valores a year y manufacture
+void Car::setCar(int d{
+	door = ;d
+	
+}
+// metodo get que return el año
+int Vehicle::getDoor(){
+	return door;
+}
+// metodo get que return manufacture
+string Vehicle::getDoor(){
+	return door;
+}
+void Car::displayInfo(){
+	cout <<" information Vehicle "<<endl;
+	cout << " Manufacture " << manufacture<<endl;
+	cout << " aÒo de build "<< year<<endl;
+	cout << " numbber the door" <<door <<endl;
+
+
+}
 
 
 
--- Vehicle.h
+++ Vehicle.h
@@ -1,20 +1,39 @@
-#include <string>
-using namespace std;
 #include <iostream>
-class Vechicle {
-
-public: 
-int year;
-string manufacturer;
-
-Vechicle(){
-
-
+using namespace std;
+class Vehicle{
+	public:
+		int year;
+		string manufacture;
+	public:
+		Vehicle(); // constgructor vacio
+		// seter asignamos valores 
+		void setVehicle(int,string);
+		// geter retorna valores
+		int getYear();
+		string getManufacture();
+		void displayInfo();
+		
+};
+Vehicle::Vehicle(){ // constructor vacio
+}
+// seter asignamos valores a year y manufacture
+void Vehicle::setVehicle(int y, string m){
+	year = y;
+	manufacture =m;
+}
+// metodo get que return el aÒo
+int Vehicle::getYear(){
+	return year;
+}
+// metodo get que return manufacture
+string Vehicle::getManufacture(){
+	return manufacture;
+}
+void Vehicle::displayInfo(){
+	cout <<" information Vehicle "<<endl;
+	cout << " Manufacture " << manufacture<<endl;
+	cout << " aÒo de build "<< year<<endl;
+	
 
 }
-
-
-
-
-};
-
+;
